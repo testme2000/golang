45a6204933288aa0485421892f2fc14b2954c5f1
testme2000 <nitin.dixfree@gmail.com>
2024-02-11 13:29:09 -0600

    some extra go quiz

--- golang/some intersting/go_zerofirst.go
+++ golang/some intersting/go_zerofirst.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Zero First: Move all zero to start of array without changing the order")
-	var arr []int = []int{2, 3, 0, 0, 4, 5, 0}
+	var arr []int = []int{2, 3, 0, 0, 0, 5, 4}
 	var start int = len(arr) - 1
 	end := len(arr) - 1
 	// Original array
@@ -12,17 +12,12 @@ func main() {
 	for start >= 0 {
 		if arr[start] == 0 {
 			start -= 1
-			fmt.Println("Start", start)
 		} else {
-			fmt.Println("Start", start)
-			fmt.Println("End", end)
-			fmt.Println(arr)
 			temp := arr[start]
 			arr[start] = arr[end]
 			arr[end] = temp
 			start -= 1
 			end -= 1
-			fmt.Println(arr)
 		}
 	}
 	fmt.Println(arr)
